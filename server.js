1607ecd7db12bdad7ac3c39b7bef252f97db93d9
Athira S <athiraswarrier6@gmail.com>
2026-07-24 20:59:46 +0530

    Completed Day 6 - Add Blog API using Express.js

--- server.js
+++ server.js
@@ -3,10 +3,11 @@ const path = require("path");
 
 const app = express();
 const PORT = 3000;
-
-// Serve CSS and JS from public folder
+const blogs = [];
 app.use(express.static(path.join(__dirname, "public")));
 app.use(express.urlencoded({ extended: true }));
+// Serve CSS and JS from public folder
+
 // Home page
 app.get("/", (req, res) => {
     res.sendFile(path.join(__dirname, "views", "index.html"));
@@ -19,9 +20,15 @@ app.get("/add-blog.html", (req, res) => {
 app.post("/add-blog", (req, res) => {
     const { title, author, content } = req.body;
 
-    console.log("Title:", title);
-    console.log("Author:", author);
-    console.log("Content:", content);
+   const blog = {
+        title,
+        author,
+        content
+    };
+
+    blogs.push(blog);
+
+    console.log(blogs);
 
     res.send("Blog submitted successfully!");
 });
