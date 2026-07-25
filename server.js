9060f680e95907dae4ff3199bcee7f8350962ce6
Athira S <athiraswarrier6@gmail.com>
2026-07-25 20:19:57 +0530

    Completed Day 7 - Display blogs on Home page

--- server.js
+++ server.js
@@ -3,24 +3,79 @@ const path = require("path");
 
 const app = express();
 const PORT = 3000;
+
+// Store blogs in an array
 const blogs = [];
+
+// Middleware
 app.use(express.static(path.join(__dirname, "public")));
 app.use(express.urlencoded({ extended: true }));
-// Serve CSS and JS from public folder
 
-// Home page
+// Home Page
 app.get("/", (req, res) => {
-    res.sendFile(path.join(__dirname, "views", "index.html"));
+
+    let blogHTML = "";
+
+    blogs.forEach((blog) => {
+        blogHTML += `
+            <article>
+                <h3>${blog.title}</h3>
+                <p><strong>Author:</strong> ${blog.author}</p>
+                <p>${blog.content}</p>
+            </article>
+        `;
+    });
+
+    res.send(`
+    <!DOCTYPE html>
+    <html>
+    <head>
+        <title>Blog Management System</title>
+        <link rel="stylesheet" href="/style.css">
+    </head>
+    <body>
+
+        <header>
+            <h1>Blog Management System</h1>
+
+            <nav>
+                <a href="/">Home</a> |
+                <a href="/add-blog.html">Add Blog</a>
+            </nav>
+        </header>
+
+        <main>
+
+            <h2>Welcome to Blog Management System</h2>
+            <p>Read and share amazing blogs.</p>
+
+            <h2>Latest Blog Posts</h2>
+
+            ${blogHTML}
+
+        </main>
+
+        <footer>
+            <p>&copy; 2026 Blog Management System. All Rights Reserved.</p>
+        </footer>
+
+    </body>
+    </html>
+    `);
+
 });
 
-// Add Blog page
+// Add Blog Page
 app.get("/add-blog.html", (req, res) => {
     res.sendFile(path.join(__dirname, "views", "add-blog.html"));
 });
+
+// Add Blog API
 app.post("/add-blog", (req, res) => {
+
     const { title, author, content } = req.body;
 
-   const blog = {
+    const blog = {
         title,
         author,
         content
@@ -30,7 +85,8 @@ app.post("/add-blog", (req, res) => {
 
     console.log(blogs);
 
-    res.send("Blog submitted successfully!");
+    res.redirect("/");
+
 });
 
 app.listen(PORT, () => {
